fe0b9643f2a6d5744092d97e915c665c16948d15
David Neal <drumming4eva@hotmail.com>
2015-03-15 21:35:15 +0000

    Add media queries for gravatar images

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -11,48 +11,48 @@
 		        </div>
 		        <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
 		    </form>
-				<table class="table table-striped table-hover">
-				  <thead>
-				    <tr>
-				      <th>#</th>
-				      <th>Last Name</th>
-				      <th>Address 1</th>
-				      <th>Town</th>
-				      <th>Postcode</th>
-				      <th></th>
-				    </tr>
-				  </thead>
-				  <tbody>
-					  	@if(count($customers) === 0)
-					  		<tr class="text-center warning">
-						  		<td colspan="6">
-							  		No customers found
-							  		@if( ! empty($search))
-							  			for '<b>{{ $search }}</b>'
-							  		@endif
-						  		</td>
-					  		</tr>
-					  	@else
-					  		@if( ! empty($search))
-					  			<tr class="text-center success">
-						  		<td colspan="6">
-							  		{!! $customers->total() !!} customers found for '<b>{{ $search }}</b>'
-						  		</td>
-					  		</tr>
-							@endif
-						  	@foreach($customers as $customer)
-							    <tr>
-							      <td>{{ $customer->id }}</td>
-							      <td>{{ $customer->last_name }}</td>
-							      <td>{{ $customer->address_1 }}</td>
-							      <td>{{ $customer->town }}</td>
-							      <td>{{ $customer->postcode }}</td>
-							      <td><a href="/admin/customers/edit/{{ $customer->id }}"><i class="fa fa-pencil"></i></a></td>
-							    </tr>
-							@endforeach
+			<table class="table table-striped table-hover">
+			  <thead>
+			    <tr>
+			      <th>#</th>
+			      <th>Last Name</th>
+			      <th>Address 1</th>
+			      <th>Town</th>
+			      <th>Postcode</th>
+			      <th></th>
+			    </tr>
+			  </thead>
+			  <tbody>
+				  	@if(count($customers) === 0)
+				  		<tr class="text-center warning">
+					  		<td colspan="6">
+						  		No customers found
+						  		@if( ! empty($search))
+						  			for '<b>{{ $search }}</b>'
+						  		@endif
+					  		</td>
+				  		</tr>
+				  	@else
+				  		@if( ! empty($search))
+				  			<tr class="text-center success">
+					  		<td colspan="6">
+						  		{!! $customers->total() !!} customers found for '<b>{{ $search }}</b>'
+					  		</td>
+				  		</tr>
 						@endif
-				  </tbody>
-				</table>
+					  	@foreach($customers as $customer)
+						    <tr>
+						      <td>{{ $customer->id }}</td>
+						      <td>{{ $customer->last_name }}</td>
+						      <td>{{ $customer->address_1 }}</td>
+						      <td>{{ $customer->town }}</td>
+						      <td>{{ $customer->postcode }}</td>
+						      <td><a href="/admin/customers/edit/{{ $customer->id }}"><i class="fa fa-pencil"></i></a></td>
+						    </tr>
+						@endforeach
+					@endif
+			  </tbody>
+			</table>
 			<div class="text-center">
 				{!! $customers->render() !!}
 			</div>
